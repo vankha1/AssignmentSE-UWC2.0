cedf83c03bdf98f6c3f08fa03051f97d2c539aba
vankha1 <vovankha2003@gmail.com>
2023-04-09 11:57:22 +0700

    Change database link

--- config/db/index.js
+++ config/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try{
-        await mongoose.connect('mongodb://localhost:27017/uwc_v2', {
+        await mongoose.connect('mongodb+srv://tatcatrithuc:vankha123@cluster0.pbwoixc.mongodb.net/uwc2', {
             useNewUrlParser:true,
             useUnifiedTopology: true
         });
