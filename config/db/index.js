40950cab7e86eb5d9a90419304bfe41e2cd7ee36
vankha1 <vovankha2003@gmail.com>
2023-04-18 17:35:54 +0700

    Add cloud database

--- config/db/index.js
+++ config/db/index.js
@@ -2,18 +2,8 @@
 const mongoose = require('mongoose');
 
 async function connect() {
-    // try{
-    //     await mongoose.connect('mongodb+srv://tatcatrithuc:vankha123@cluster0.pbwoixc.mongodb.net/uwc2', {
-    //         useNewUrlParser:true,
-    //         useUnifiedTopology: true
-    //     });
-    //     console.log("Connect successfully!!!")
-    // }
-    // catch{
-    //     console.log("Connect failure!!!");
-    // }
     try{
-        await mongoose.connect('mongodb://localhost:27017/uwc_v2', {
+        await mongoose.connect('mongodb+srv://tatcatrithuc:vankha123@cluster0.pbwoixc.mongodb.net/uwc2', {
             useNewUrlParser:true,
             useUnifiedTopology: true
         });
@@ -22,6 +12,16 @@ async function connect() {
     catch{
         console.log("Connect failure!!!");
     }
+    // try{
+    //     await mongoose.connect('mongodb://localhost:27017/uwc_v2', {
+    //         useNewUrlParser:true,
+    //         useUnifiedTopology: true
+    //     });
+    //     console.log("Connect successfully!!!")
+    // }
+    // catch{
+    //     console.log("Connect failure!!!");
+    // }
 }
 
 module.exports = {connect};
